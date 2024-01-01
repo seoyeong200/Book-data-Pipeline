0a793ceae419be9c5f4c60d592e9b4b52ff70725
seoyeong98 <youngseogimkk@gmail.com>
2024-01-01 19:05:10 +0900

    refactor : crawl_books method to class

--- etl/crawling/crawl.py
+++ etl/crawling/crawl.py
@@ -1,80 +1,56 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import pymysql
 
 from etl.crawling.getter import *
+from etl.crawling.mysql_dml import *
 
-db = pymysql.connect(host='localhost', port=3306, user='root', passwd='root', db='Book', charset='utf8')
-cursor = db.cursor()
-   
+class BookDataScrapper:
+   def __init__(self) -> None:
+      self.driver = webdriver.Chrome(ChromeDriverManager().install())
+      self.driver.implicitly_wait(30)
 
-def crawl_books():
-  driver = webdriver.Chrome(ChromeDriverManager().install())
-  driver.implicitly_wait(30)
+   def crawl_books(self):
+      category = get_category()
 
-  category = get_category()
+      for code in category:
+        # if code=='소설': continue
+        print (f"현재 카테고리 이름: {code}")
+        for page in range(1,3):
+            url = category[code]+'&tab=top100&list_type=list&sort_type=publishday&page={page}'.format(page=page)
+            # 네이버의 베스트셀러 웹페이지 정보
+            self.driver.get(url)
+            bsObject = BeautifulSoup(self.driver.page_source, 'html.parser')
 
-  for code in category:
-    # if code=='소설': continue
-    print (f"현재 카테고리 이름: {code}")
-    for page in range(1,3):
-        url = category[code]+'&tab=top100&list_type=list&sort_type=publishday&page={page}'.format(page=page)
-        # 네이버의 베스트셀러 웹페이지 정보
-        driver.get(url)
-        bsObject = BeautifulSoup(driver.page_source, 'html.parser')
+            book_page_urls = get_book_page_urls(bsObject)
+            
+            #리스트로 받아온 책의 각 세부 url 안에 들어가서 순위, 제목, 저자, 이미지, 책내용 받아오기
+            for index, book_page_url in enumerate(book_page_urls):
+                self.driver.get(book_page_url)
+                bid = book_page_url.split("bid=")[1]
 
-        book_page_urls = get_book_page_urls(bsObject)
-        
-        #리스트로 받아온 책의 각 세부 url 안에 들어가서 순위, 제목, 저자, 이미지, 책내용 받아오기
-        for index, book_page_url in enumerate(book_page_urls):
-            driver.get(book_page_url)
-            bid = book_page_url.split("bid=")[1]
+                sql = """SELECT COUNT(*) FROM items WHERE book_id = '""" + bid + """';"""
+                cursor.execute(sql)
+                result = cursor.fetchone()
 
-            sql = """SELECT COUNT(*) FROM items WHERE book_id = '""" + bid + """';"""
-            cursor.execute(sql)
-            result = cursor.fetchone()
-
-            if result[0] == 0: # bid가 sql에 이미 저장되어있지 않으면 세부내용스크래핑 start
-                bsObject = BeautifulSoup(driver.page_source, 'html.parser')
-                title, image, description, url, author = get_book_data(bsObject)
-                try:
-                  save_data({'bid':bid, 
-                             'title':title, 
-                             'author':"'"+author+"'", 
-                             'image':"'"+image+"'", 
-                             'rank':str(20*(page-1)+index+1), 
-                             'description':description, 
-                             'category':code})
-                except:
-                  print(title, "sql 실패")
-
-
-# mysql에 책 하나 저장
-def save_data(item_info):
-    # print (item_info)
-    # COUNT : item_code 중복으로 인한 오류 예방 : item_code가 이미 존재하면 해당 책은 insert하지 않음
-    # sql = """SELECT COUNT(*) FROM items WHERE book_id = '""" + item_info['bid'] + """';"""
-    # cursor.execute(sql)
-    # result = cursor.fetchone()
-    # if result[0] == 0:
-    sql = """INSERT INTO items VALUES('""" + item_info['bid'] + """',
-    '""" + item_info['title'] + """', 
-    """ + item_info['author'] + """, 
-    """ + item_info['image'] + """, 
-    '""" + item_info['rank'] + """',
-    '""" + item_info['description'] + """',
-    '""" + item_info['category'] + """',
-    now()
-    );"""
-    print (sql)
-    cursor.execute(sql)
-
-    db.commit()
-
+                if result[0] == 0: # bid가 sql에 이미 저장되어있지 않으면 세부내용스크래핑 start
+                    bsObject = BeautifulSoup(self.driver.page_source, 'html.parser')
+                    title, image, description, url, author = get_book_data(bsObject)
+                    try:
+                      save_data({'bid':bid, 
+                                'title':title, 
+                                'author':"'"+author+"'", 
+                                'image':"'"+image+"'", 
+                                'rank':str(20*(page-1)+index+1), 
+                                'description':description, 
+                                'category':code})
+                    except:
+                      print(title, "sql 실패")
 
 if __name__=="__main__":
-  crawl_books()
+  scrapper = BookDataScrapper()
+  scrapper.crawl_books()
+
 
 
 
