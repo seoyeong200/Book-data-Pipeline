c5836af481db40ae95af8cfcb978a1a84fce2428
seoyeong98 <youngseogimkk@gmail.com>
2024-01-01 19:35:11 +0900

    fix : typo

--- infrastructure/sqoop_import.sh
+++ infrastructure/sqoop_import.sh
@@ -6,13 +6,13 @@ DATE=$2
 # check arguments
 if [-z "${DATE}"]; then
     sqoop import -jt local -fs local -m 1 \
-        --connect jdbc:mysql://mysql:3306/Book --username root --password root --table items \
+        --connect jdbc:mysql://mysql:3306/Book --username root --password root --table $TABLE \
         --where "curdate() > $DATE" \
         --target-dir /tmp/target/book_table/$($DATE '+%Y_%m_%d') \
         --as-parquetfile
 else
     sqoop import -jt local -fs local -m 1 \
-        --connect jdbc:mysql://mysql:3306/Book --username root --password root --table items \
+        --connect jdbc:mysql://mysql:3306/Book --username root --password root --table $TABLE \
         --target-dir /tmp/target/book_table/total \
         --as-parquetfile
 fi
