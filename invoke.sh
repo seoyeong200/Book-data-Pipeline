b9cb1624fe47cd65f09e61e440eb4a20ba6bebcb
seoyeong98 <youngseogimkk@gmail.com>
2024-01-23 19:16:58 +0900

    fix : 스크래퍼 실행시키는 잡을 실행시키는 로직

--- invoke.sh
+++ invoke.sh
@@ -2,9 +2,35 @@
 DOCKER_IMAGE="docker-image"
 DOCKER_TAG="test"
 PORT=8080
+CONTAINER_NAME="scrapper-container"
+CATEGORY_LIST_JSON_PATH="./src/etl/utils/static/book_category_url.json"
 
 echo "docker local test"
-docker build -t $DOCKER_IMAGE:$DOCKER_TAG .
-docker run -d -p ${PORT}:${PORT} --env-file .env ${DOCKER_IMAGE}:${DOCKER_TAG}
 
-curl "http://localhost:${PORT}/2015-03-31/functions/function/invocations" -d '{}'
+# docker-image:test 이미지 기반으로 컨테이너 exec
+# docker build -t $DOCKER_IMAGE:$DOCKER_TAG .
+
+category_lists=($(jq ". | keys[]" ${CATEGORY_LIST_JSON_PATH}))
+
+for c in ${category_lists[@]}; do
+    echo $c
+    docker run -d \
+        -p ${PORT}:${PORT} \
+        --env-file .env \
+        -v ./src/etl:/var/task \
+        --name ${CONTAINER_NAME} \
+        ${DOCKER_IMAGE}:${DOCKER_TAG} \
+
+    tmp="${c%\"}"
+    tmp="${tmp#\"}"
+
+    curl "http://localhost:8080/2015-03-31/functions/function/invocations" -d "{\"category\": [\"$tmp\"]}"
+
+    docker stop ${CONTAINER_NAME} 
+    docker rm ${CONTAINER_NAME} 
+done
+
+'''
+docker run -d -p 8080:8080 --env-file .env -v ./src/etl:/var/task docker-image:test
+curl "http://localhost:8080/2015-03-31/functions/function/invocations" -d '{"category" : ["취업", "IT"]}'
+'''
