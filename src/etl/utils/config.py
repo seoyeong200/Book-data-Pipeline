1023ec6784a06596e3a58be802b789f82801821d
seoyeong98 <youngseogimkk@gmail.com>
2024-01-23 19:17:43 +0900

    fix : 컨테이너 내 파일 경로 못 찾는 부분 수정

--- src/etl/utils/config.py
+++ src/etl/utils/config.py
@@ -1,9 +1,12 @@
-import os
-
 def get_workdir():
+    import os
     """
     return project abs path string
     """
-    if os.environ.get('PYTHONPATH') == "'/var/task'":
+    if os.environ.get('PYTHONPATH') == '/var/task':
         return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     return os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir), os.pardir))
+
+def get_date():
+    from datetime import date
+    return date.today()
