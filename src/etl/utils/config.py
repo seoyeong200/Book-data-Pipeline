ee4fba07681533b3d05524c8088f194eafe2edbc
seoyeong98 <youngseogimkk@gmail.com>
2024-01-23 15:15:06 +0900

    fix : workdir 받아오는 부분 수정(by system)

--- src/etl/utils/config.py
+++ src/etl/utils/config.py
@@ -4,4 +4,6 @@ def get_workdir():
     """
     return project abs path string
     """
+    if os.environ.get('PYTHONPATH') == "'/var/task'":
+        return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     return os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir), os.pardir))
