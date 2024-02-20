1562119de3bd626c870807aaa30910d69f79632a
seoyeong98 <youngseogimkk@gmail.com>
2024-02-20 19:03:39 +0900

    test : 레코드 하나 당 비슷한 책 갖고오는 부분 실행 테스트

--- src/etl/apps/word2vec.py
+++ src/etl/apps/word2vec.py
@@ -1,5 +1,8 @@
-from pyspark.ml.feature import Word2Vec
+from pyspark.ml.feature import Word2Vec as W2V
 from pyspark.ml.feature import Word2VecModel
+from pyspark.ml.linalg import DenseVector
+
+from pyspark.sql import DataFrame
 from pyspark.sql.types import FloatType
 from pyspark.sql.window import Window
 import pyspark.sql.functions as F
@@ -7,12 +10,12 @@ import pyspark.sql.functions as F
 class Word2Vec:
     def __init__(self, df) -> None:
         self.df = df
-        self.w2v_model_path = './model_w2v' #TODO make it to absolute path
+        self.w2v_model_path = '/opt/spark-data/model_w2v'
         self.inputCol = "preprocessed"
         self.outputCol = "vectorized"
 
     def train_model(self):
-        w2v = Word2Vec(
+        w2v = W2V(
             vectorSize=100, 
             maxIter=10, 
             minCount=5, 
@@ -21,13 +24,24 @@ class Word2Vec:
             outputCol=self.outputCol
         )
         model_with_preprocessed_col = w2v.fit(self.df)
+        print(model_with_preprocessed_col.getVectors().show())
         model_with_preprocessed_col.save(self.w2v_model_path)    
+
+    def calculate(self):
+        #TODO iterate each vectorized_df row = record
+        record = self.df.first()
+        print(f"take one record\n{record}")
+        similarity_with_record_df = self.calcuate_simiarity(record[self.outputCol])
+        print(f'calculate each similarity between record and every records\n{similarity_with_record_df}')
+        similar_book_with_record_list = self.get_similar_books(similarity_with_record_df)
+        print(f'get top 10 similar book lists of record\n{similar_book_with_record_list}')
+        return similar_book_with_record_list
 
     def get_vectorized_df(self):
         loaded_w2v = Word2VecModel.load(self.w2v_model_path)
         return loaded_w2v.transform(self.df)
     
-    def calcuate_simiarity(self, static_vector):
+    def calcuate_simiarity(self, static_vector: DenseVector) -> DataFrame:
         
         def cos_sim(a,b):
             import numpy as np
@@ -41,7 +55,7 @@ class Word2Vec:
             )
         )
     
-    def get_similar_books(self, df_with_cos_sim):
+    def get_similar_books(self, df_with_cos_sim: DataFrame) -> list:
         window_with_category = (
             Window
             .partitionBy(df_with_cos_sim['category'])
@@ -51,8 +65,8 @@ class Word2Vec:
             df_with_cos_sim
             .select('*', F.rank().over(window_with_category).alias('similarity_rank'))
             .filter(
-                (F.col('similarity_rank') > 1) & (F.col('similarity_rank') <= 11)
+                (F.col('similarity_rank') > 1) & (F.col('similarity_rank') <= 6)
             )
         )
-        return result.select('bid')
+        return result.select('bid').rdd.flatMap(lambda x: x).collect()
 
