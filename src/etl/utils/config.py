e98267c9581496c77d4331d9cb91e81d7a76b6b3
seoyeong98 <youngseogimkk@gmail.com>
2024-03-26 19:01:43 +0900

    fix : scrapper 동작 기준 - same week 로직 오류 수정

--- src/etl/utils/config.py
+++ src/etl/utils/config.py
@@ -4,7 +4,8 @@ def get_workdir():
     return project abs path string
     """
     if os.environ.get('PYTHONPATH') == '/var/task':
-        return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        return '/var/task'
+        # return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     return os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir), os.pardir))
 
 def get_date():
@@ -12,9 +13,14 @@ def get_date():
     return str(date.today())
 
 
-def is_same_week(dateString):
+def is_same_week(dateString: str) -> bool:
+    """
+    Return whether input dateString is in the same week
+    as the date of this function called.
+    """
     import datetime
     d1 = datetime.datetime.strptime(dateString,'%Y-%m-%d')
     d2 = datetime.datetime.today()
+    print(f"input date={d1}, today={d2}")
     return d1.isocalendar()[1] == d2.isocalendar()[1] \
               and d1.year == d2.year
