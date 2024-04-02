dbdba987e5e243dc8720085803af5a6d6f949247
seoyeong98 <youngseogimkk@gmail.com>
2024-04-02 19:08:24 +0900

    fix : get_workdir 버그

--- src/etl/utils/config.py
+++ src/etl/utils/config.py
@@ -4,8 +4,7 @@ def get_workdir():
     return project abs path string
     """
     if os.environ.get('PYTHONPATH') == '/var/task':
-        return '/var/task'
-        # return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     return os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir), os.pardir))
 
 def get_date():
