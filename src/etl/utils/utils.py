9201b207656576935f7afbb82e18307f40754b5b
seoyeong200 <youngseogimkk@gmail.com>
2024-06-03 20:40:00 +0900

    fix : insert calculated similar book pair to db

--- src/etl/utils/utils.py
+++ src/etl/utils/utils.py
@@ -4,7 +4,7 @@ def is_same_week(dateString: str) -> bool:
     as the date of this function called.
     """
     import datetime
-    
+
     d1 = datetime.datetime.strptime(dateString,'%Y-%m-%d')
     d2 = datetime.datetime.today()
     print(f"input date={d1}, today={d2}")
@@ -30,6 +30,36 @@ def init_spark():
     .config('spark.jars.packages', '/opt/spark/jars/postgresql-42.7.3.jar')\
     .getOrCreate()
   return spark
+
+
+def write_local(df, df_name):
+   df.write.parquet(f'/opt/spark-data/{df_name}.parquet')
+
+
+def write_s3(df):
+   import os
+   s3_uri = os.path.join(os.environ['AWS_S3_PATH'], 'processed_data')
+   
+   df.write \
+    .mode('overwrite') \
+    .parquet(path=s3_uri)
+   
+
+
+def write_postgre(df, tablename):
+   import os
+  
+   username, password, dbname = os.environ['POSTGRES_USER'], os.environ['POSTGRES_PASSWORD'], os.environ['POSTGRES_DB']
+   
+   df.select("*")\
+      .write \
+      .format("jdbc") \
+      .option("url", f"jdbc:postgresql://postgreDB:5432/{dbname}") \
+      .option("driver", "org.postgresql.Driver") \
+      .option("dbtable", tablename) \
+      .option("user", username) \
+      .option("password", password) \
+      .save() 
 
 
 def get_args():
